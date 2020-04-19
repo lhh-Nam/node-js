9fc70e695ead1b5ac78e3310918b81a49afd9923
lhh.Nam <lehoanghuynam0401@gmail.com>
2020-04-19 21:33:07 +0700

    Add & express router &  controller MVC & template layout

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var express = require("express");
-var app = express();
+var bodyParser = require("body-parser");
+
+var userRoute = require("./routes/user.route");
 
 var port = 3000;
 
+var app = express();
 app.set("view engine", "pug");
 app.set("views", "./views");
 
-var users = [
-  { id: 1, name: "Nhu" },
-  { id: 2, name: "Nam" },
-];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.render("index", {
@@ -17,23 +18,7 @@ app.get("/", function (req, res) {
   });
 });
 
-app.get("/users", function (req, res) {
-  res.render("users/index", {
-    users: users,
-  });
-});
-
-app.get("/users/search", function (req, res) {
-  var q = req.query.q;
-  var matchedUsers = users.filter(function (user) {
-    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-  });
-
-  res.render("users/index", {
-    users: matchedUsers,
-    question: q,
-  });
-});
+app.use("/users", userRoute);
 
 app.listen(port, function () {
   console.log("Server listening on port " + port);
