71917adf8fc4b4badaf19732d9729f749887618e
lhh.Nam <lehoanghuynam0401@gmail.com>
2020-04-21 13:32:18 +0700

    Add cookie & Authentication

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 
 var userRoute = require("./routes/user.route");
+var authRoute = require("./routes/auth.route");
+
+var authMiddleware = require("./middlewares/auth.middleware");
 
 var port = 3000;
 
@@ -11,6 +15,7 @@ app.set("views", "./views");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(express.static("public"));
 
@@ -20,7 +25,8 @@ app.get("/", function (req, res) {
   });
 });
 
-app.use("/users", userRoute);
+app.use("/users", authMiddleware.requireAuth, userRoute);
+app.use("/auth", authRoute);
 
 app.listen(port, function () {
   console.log("Server listening on port " + port);
